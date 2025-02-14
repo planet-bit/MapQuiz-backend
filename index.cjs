d03c399ba65bd2153842b7c84db8b42ee4ffa23c
Kuroki <978508617ma@gmail.com>
2025-02-14 13:49:42 +0900

    初回コミット（backend）

---
+++ index.cjs
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello from MapQuiz Backend');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
