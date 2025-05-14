8f153286540cb74930ba205d388bec794ce3a7a4
Kuroki <978508617ma@gmail.com>
2025-05-14 09:14:38 +0900

    db.cjs更新

--- db.cjs
+++ db.cjs
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise"); // promise ベースのmysql2を使う
 require("dotenv").config({ path: '.env' });
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "mapquiz-mysql-container",
+  host: process.env.DB_HOST || "mapquiz-mysql",
   user: process.env.DB_USER || "user",
   password: process.env.DB_PASSWORD || "password",
   database: process.env.DB_NAME || "mapquizdb",
